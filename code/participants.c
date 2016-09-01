9d2e2887627265a5955c1485d77676b6e6008a8b
arwolowi <arwolowi@mtu.edu>
2016-09-01 13:21:28 -0400

    Added code for Andrew Wolowiec

--- code/participants.c
+++ code/participants.c
@@ -22,6 +22,7 @@ main (int argc, char *argv[])
   printf ("%02d: Kim Tracy\n", ++i);
   printf ("%02d: Justin Williams\n", ++i);
   printf ("%02d: Steven Bertolucci\n", ++i);
+  printf ("%02d: Andrew Wolowiec\n", ++i);
   
   
   printf ("There are %i participants.\n", i);
