7518b64c64d1bae5ed13ff3520eab8a07efd1996
Baron Lenardson <blenardson@Lucky.Laptop>
2016-09-02 11:18:57 -0400

    Added Print statement to code for Baron Lenardson

--- code/participants.c
+++ code/participants.c
@@ -23,7 +23,7 @@ main (int argc, char *argv[])
   printf ("%02d: Justin Williams\n", ++i);
   printf ("%02d: Steven Bertolucci\n", ++i);
   printf ("%02d: Andrew Wolowiec\n", ++i);
-  
+  printf ("%02d: Baron Lenardson\n", ++i);
   
   printf ("There are %i participants.\n", i);
   return 0;
