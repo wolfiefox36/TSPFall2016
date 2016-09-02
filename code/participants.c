76de28cc43c1f41836e2a2b2d074a121e0d4b2fe
DifferentUserName <jvprewet@mtu.edu>
2016-09-02 14:36:48 -0400

    Added line for Joshua Prewett

--- code/participants.c
+++ code/participants.c
@@ -26,6 +26,7 @@ main (int argc, char *argv[])
   printf ("%02d: Ben Closner\n", ++i); 
   printf ("%02d: Baron Lenardson\n", ++i);
   printf ("%02d: Jordan Gagnon\n", ++i);
+  printf ("%02d: Joshua Prewett\n", ++i);
   
   printf ("There are %i participants.\n", i);
   return 0;
